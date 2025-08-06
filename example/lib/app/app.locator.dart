f18365e16fae9457255e91bc21047bb08bbca1fc
Eimen2018 <nur.amuni26@gmail.com>
2025-08-07 02:34:55 +0400

    chore: update README and Dart files for improved submodule management and code organization

--- example/lib/app/app.locator.dart
+++ example/lib/app/app.locator.dart
@@ -6,10 +6,10 @@
 
 // ignore_for_file: public_member_api_docs, implementation_imports, depend_on_referenced_packages
 
-import 'package:stacked_core/stacked_core.dart';
 import 'package:stacked_services/src/bottom_sheet/bottom_sheet_service.dart';
 import 'package:stacked_services/src/dialog/dialog_service.dart';
 import 'package:stacked_services/src/navigation/navigation_service.dart';
+import 'package:stacked_shared/stacked_shared.dart';
 import 'package:stacked_themes/src/theme_service.dart';
 
 import '../services/epoch_service.dart';
@@ -20,8 +20,10 @@ import '../ui/bottom_nav/history/history_viewmodel.dart';
 
 final exampleLocator = StackedLocator.instance;
 
-Future<void> setupExampleLocator(
-    {String? environment, EnvironmentFilter? environmentFilter}) async {
+Future<void> setupExampleLocator({
+  String? environment,
+  EnvironmentFilter? environmentFilter,
+}) async {
 // Register environments
   exampleLocator.registerEnvironment(
       environment: environment, environmentFilter: environmentFilter);
